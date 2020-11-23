ceb4d48ea47898bdab8c71d770ecf902d4f4bbc5
Luis Jeria G <luisjeriagarrido@gmail.com>
2020-11-23 14:37:14 -0300

    Trabajo Evaluacion 3
    -Se crea MainActivity con funcionalidad solicitada, Spinner, Boton y
    ListWiew
    -Se llena el ListView con un endpoint de Los Simpsons segun indicaciones
    en Spinnner apretando el Boton
    -Se utiliza Gson y Volley para conversion de Arreglo JSON y Piccaso para
    renderizacion de imaganes
    -Se realiza Validacion para que se escoja un valor del Spinnner
    
    Autor Luis Jeria

---
+++ Fuentes/app/src/main/java/cl/inacap/simpsonsapp/MainActivity.java
@@ -0,0 +1,121 @@
+package cl.inacap.simpsonsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import cl.inacap.simpsonsapp.adapters.FrasesListAdapter;
+import cl.inacap.simpsonsapp.dto.Frase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner frasesSpinner;
+    private Button ejecutarBtn;
+    private ListView frasesLv;
+    private RequestQueue requestQueue;
+    private List<Frase> frases = new ArrayList<>();
+    private FrasesListAdapter flAdapter;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.frasesSpinner = findViewById(R.id.cantidad_spinner);
+        this.ejecutarBtn = findViewById(R.id.ejecutar_btn);
+        this.frasesLv = findViewById(R.id.frases_lv);
+
+        this.flAdapter = new FrasesListAdapter(this, R.layout.frases_list, this.frases);
+        this.frasesLv.setAdapter(flAdapter);
+
+        ArrayList<String> cantidadFrases = new ArrayList<>();
+        for(int i=0;i<11;i++){
+            if(i==0){
+                cantidadFrases.add("------Seleccione la cantidad de consejos------");
+            }else if(i==1){
+                cantidadFrases.add("1 Consejo");
+            }else{
+                cantidadFrases.add(i + " Consejos");
+            }
+        }
+
+        ArrayAdapter spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,cantidadFrases);
+        this.frasesSpinner.setAdapter(spinnerAdapter);
+
+        this.frasesSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                parent.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        this.ejecutarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num = frasesSpinner.getSelectedItemPosition();
+                if(num==0){
+                    Toast.makeText(MainActivity.this,"Debe seleccionar cantidad de consejos",Toast.LENGTH_LONG).show();
+                }else{
+                    String url = ("https://thesimpsonsquoteapi.glitch.me/quotes?count="+ num);
+                    requestQueue = Volley.newRequestQueue(MainActivity.this);
+                    JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                            new Response.Listener<JSONArray>() {
+                                @Override
+                                public void onResponse(JSONArray response) {
+                                    try{
+                                        frases.clear();
+                                        Frase[] fraseObt =new Gson().fromJson(response.toString(),Frase[].class);
+                                        frases.addAll(Arrays.asList(fraseObt));
+                                    }catch(Exception ex){
+                                        frases=null;
+                                    }finally {
+                                        flAdapter.notifyDataSetChanged();
+                                    }
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            frases=null;
+                            flAdapter.notifyDataSetChanged();
+                        }
+                    });
+                    requestQueue.add(jsonArrayRequest);
+                }
+            }
+        });
+
+    }
+
+
+
+}
---
+++ Fuentes/app/src/main/java/cl/inacap/simpsonsapp/adapters/FrasesListAdapter.java
@@ -0,0 +1,52 @@
+package cl.inacap.simpsonsapp.adapters;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+import java.util.zip.Inflater;
+
+import cl.inacap.simpsonsapp.R;
+import cl.inacap.simpsonsapp.dto.Frase;
+
+public class FrasesListAdapter extends ArrayAdapter<Frase> {
+
+    private List<Frase> frases;
+    private Activity activity;
+
+    public FrasesListAdapter(@NonNull Activity context, int resource, @NonNull List<Frase> objects) {
+        super(context, resource, objects);
+        this.frases = objects;
+        this.activity = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        LayoutInflater inflater = this.activity.getLayoutInflater();
+        View item = inflater.inflate(R.layout.frases_list,null,true);
+        TextView fraseTxt = item.findViewById(R.id.frase_personaje);
+        TextView nombreTxt = item.findViewById(R.id.nombre_personaje);
+        ImageView imagenTxt = item.findViewById(R.id.imagen_personaje);
+
+        fraseTxt.setText("\""+ this.frases.get(position).getQuote() + "\"");
+        nombreTxt.setText("-" + this.frases.get(position).getCharacter());
+        Picasso.get().load(this.frases.get(position).getImage())
+                .resize(300,600).centerInside().into(imagenTxt);
+        return item;
+
+    }
+}
